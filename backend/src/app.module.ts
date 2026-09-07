9d691220c3cd6f517ea38211379b94c3c6bc93bc
SOMNATH SARKAR <somnath20005.sarkar@gmail.com>
2026-09-07 21:39:51 +0530

    feat: register source ingestion module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
-import { Module } from "@nestjs/common";
-import { TransformController } from "./transform.controller";
-import { ExportController } from "./export.controller";
-import { TransformService } from "./transform.service";
-
-@Module({ controllers: [TransformController, ExportController], providers: [TransformService] })
+import { Module } from '@nestjs/common';
+import { TransformController } from './transform.controller';
+import { ExportController } from './export.controller';
+import { SourceController } from './source.controller';
+import { TransformService } from './transform.service';
+import { SourceService } from './source.service';
+@Module({controllers:[TransformController,ExportController,SourceController],providers:[TransformService,SourceService]})
 export class AppModule {}
