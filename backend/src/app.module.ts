1cca211b1e63a648b98ece56f1d08cc239b56aa0
SOMNATH SARKAR <somnath20005.sarkar@gmail.com>
2026-09-07 21:45:04 +0530

    feat: register workflow controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,8 +4,13 @@ import { ExportController } from './export.controller';
 import { SourceController } from './source.controller';
 import { UploadController } from './upload.controller';
 import { ResearchController } from './research.controller';
+import { WorkflowController } from './workflow.controller';
 import { TransformService } from './transform.service';
 import { SourceService } from './source.service';
 import { ResearchService } from './research.service';
-@Module({controllers:[TransformController,ExportController,SourceController,UploadController,ResearchController],providers:[TransformService,SourceService,ResearchService]})
+
+@Module({
+  controllers: [TransformController, ExportController, SourceController, UploadController, ResearchController, WorkflowController],
+  providers: [TransformService, SourceService, ResearchService],
+})
 export class AppModule {}
