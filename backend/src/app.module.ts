76b441cd818565ae008722593e229e26e79b5ece
SOMNATH SARKAR <somnath20005.sarkar@gmail.com>
2026-09-07 21:40:55 +0530

    feat: wire upload and research modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { TransformController } from './transform.controller';
 import { ExportController } from './export.controller';
 import { SourceController } from './source.controller';
+import { UploadController } from './upload.controller';
+import { ResearchController } from './research.controller';
 import { TransformService } from './transform.service';
 import { SourceService } from './source.service';
-@Module({controllers:[TransformController,ExportController,SourceController],providers:[TransformService,SourceService]})
+import { ResearchService } from './research.service';
+@Module({controllers:[TransformController,ExportController,SourceController,UploadController,ResearchController],providers:[TransformService,SourceService,ResearchService]})
 export class AppModule {}
